a65e903bc3f7ddabbbe75b8096cbd3d5e224f9bc
michelle <kazekage422@gmail.com>
2018-03-27 11:20:49 +0200

    commit met update van ServicesConfig

--- src/main/java/be/vdab/services/ServicesConfig.java
+++ src/main/java/be/vdab/services/ServicesConfig.java
@@ -1,12 +1,14 @@
 package be.vdab.services;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import be.vdab.repositories.PersoonRepository;
 
 @Configuration
 public class ServicesConfig {
+	@Bean
 	PersoonService persoonService(
 			@Qualifier("CSV") PersoonRepository persoonRepository) {
 		return new PersoonServiceImpl(persoonRepository);
